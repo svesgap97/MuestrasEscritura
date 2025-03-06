aeeccd701e8ad4ae453fc56a643e7003958fa3ba
svesgap97 <svrap247@gmail.com>
2025-03-05 19:38:23 -0500

    Primera Versión

--- data/serie.config.js
+++ data/serie.config.js
@@ -1,38 +1,74 @@
 const config = {
   "lang": "es",
   "localPort": "5173",
-  "base": "https://srsergiorodriguez.github.io",
-  "baseurl": "/serie-mini",
-  "title": "Serie Mini",
-  "subtitle": "Un sistema para mini colecciones digitales",
-  "credits": "Por Sergio Rodríguez Gómez",
-  "copyright": "Todos los derechos reservados, 2024",
-  "logos": [
-    {img: "logos/logo.png", link: "https://github.com/srsergiorodriguez/serie-mini"}
-  ],
+  "base": "https://svesgap97.github.io",
+  "baseurl": "/MuestrasEscritura",
+  "title": "Muestras7A",
+  "subtitle": "primeramuestra",
+  "credits": "santiagovesga",
+  "copyright": "colegiolacolina2025",
   "pages": {
     "iiifViewer": true,
     "metadataToShow": [
       {
-        "key": "label",
-        "label": "Nombre",
+        "key": "TYPE",
+        "label": "TYPE",
         "type": "text"
       },
       {
-        "key": "autor",
-        "label": "Autor",
+        "key": "FORMAT",
+        "label": "FORMAT",
         "type": "text"
       },
       {
-        "key": "fecha",
-        "label": "Fecha",
+        "key": "EXTENTION",
+        "label": "EXTENTION",
+        "type": "text"
+      },
+      {
+        "key": "DATE",
+        "label": "DATE",
+        "type": "text"
+      },
+      {
+        "key": "COMPRENSI�N",
+        "label": "COMPRENSI�N",
+        "type": "text"
+      },
+      {
+        "key": "ORGANIZACI�N",
+        "label": "ORGANIZACI�N",
+        "type": "text"
+      },
+      {
+        "key": "CONTENIDO",
+        "label": "CONTENIDO",
+        "type": "text"
+      },
+      {
+        "key": "LENGUAJE",
+        "label": "LENGUAJE",
+        "type": "text"
+      },
+      {
+        "key": "TOTAL",
+        "label": "TOTAL",
         "type": "text"
       }
     ],
     "metadataToIndex": [
       "label",
-      "autor",
-      "fecha"
+      "TYPE",
+      "FORMAT",
+      "EXTENTION",
+      "DATE",
+      "COMPRENSI�N",
+      "ORGANIZACI�N",
+      "CONTENIDO",
+      "LENGUAJE",
+      "TOTAL",
+      "ID_PRUEBA",
+      "ID_ESTUDIANTE"
     ]
   }
 };
